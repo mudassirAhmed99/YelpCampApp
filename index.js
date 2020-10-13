22d3d39fe508e9d93de2ecea8467275dd8198142
Mudassir Ahmed <ahmed.mudassir1999@gmail.com>
2020-10-13 08:26:47 +0530

    Initial commit of the YelpCampApp.

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const Campground = require('./models/campground');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp',{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true
+});
+
+const db = mongoose.connection;
+db.on('error',console.error.bind(console,'connection error:'));
+db.once('open',() => {
+    console.log('DATABASE CONNECTED');
+});
+
+app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'views'));
+//------------------------------------------------------------------------------
+
+app.get('/',(req,res) => {
+    res.render('home');
+})
+
+app.get('/campgrounds',async (req,res) =>{
+    const campgrounds = await Campground.find({});
+    res.render('campgrounds/index', {campgrounds});
+})
+
+app.get('/campgrounds/:id', async (req,res) =>{
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/show',{ campground });
+})
+
+app.get('/campgrounds/new', (req,res) => {
+    res.render('campgrounds/new')
+})
+
+app.listen(3000,() => {
+    console.log("SERVER IS RUNNING ON PORT 3000.")
+})
