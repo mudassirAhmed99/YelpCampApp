fd09630365ce0bcb84bd411ce604b907b50d6302
Mudassir Ahmed <ahmed.mudassir1999@gmail.com>
2020-10-15 09:00:37 +0530

    Added mongo delete validation to delete all the reviews of a deleted campground.

--- index.js
+++ index.js
@@ -54,6 +54,7 @@ const validateReview = (req, res, next) => {
 app.get('/', (req, res) => {
     res.render('home')
 });
+
 app.get('/campgrounds', catchAsync(async (req, res) => {
     const campgrounds = await Campground.find({});
     res.render('campgrounds/index', { campgrounds })
@@ -61,14 +62,13 @@ app.get('/campgrounds', catchAsync(async (req, res) => {
 
 app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
-})
-
+});
 
 app.post('/campgrounds', validateCampground, catchAsync(async (req, res, next) => {
     const campground = new Campground(req.body.campground);
     await campground.save();
     res.redirect(`/campgrounds/${campground._id}`)
-}))
+}));
 
 app.get('/campgrounds/:id', catchAsync(async (req, res,) => {
     const campground = await Campground.findById(req.params.id).populate('reviews');
@@ -78,7 +78,7 @@ app.get('/campgrounds/:id', catchAsync(async (req, res,) => {
 app.get('/campgrounds/:id/edit', catchAsync(async (req, res) => {
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/edit', { campground });
-}))
+}));
 
 app.put('/campgrounds/:id', validateCampground, catchAsync(async (req, res) => {
     const { id } = req.params;
@@ -106,7 +106,7 @@ app.delete('/campgrounds/:id/reviews/:reviewId', catchAsync(async (req, res) => 
     await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
     await Review.findByIdAndDelete(reviewId);
     res.redirect(`/campgrounds/${id}`);
-}))
+}));
 
 
 app.all('*', (req, res, next) => {
@@ -121,4 +121,4 @@ app.use((err, req, res, next) => {
 
 app.listen(3000, () => {
     console.log('Serving on port 3000')
-})
+});
