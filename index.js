0f4aa6fb64c12c421ec25b6ee4057591b4c2f471
Mudassir Ahmed <ahmed.mudassir1999@gmail.com>
2020-10-15 08:25:45 +0530

    Added a review model and connected it as an object id to the campground model.

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const app = express();
 const path = require('path');
 const mongoose = require('mongoose');
 const ejsMate = require('ejs-mate');
-const Joi = require('joi');
 const catchAsync = require('./utils/catchAsync');
 const ExpressError = require('./utils/ExpressError');
 const methodOverride = require('method-override');
