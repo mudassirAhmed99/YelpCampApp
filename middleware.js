61fb18e2727d0f428a659734f6bf385876ec5edd
Mudassir Ahmed <ahmed.mudassir1999@gmail.com>
2020-10-20 13:21:08 +0530

    Added reviews controller.

--- middleware.js
+++ middleware.js
@@ -45,7 +45,7 @@ module.exports.validateReview = (req, res, next) => {
 
 module.exports.isReviewAuthor = async(req, res, next) => {
     const { id,reviewId } = req.params;
-    const review = await Review.findById(id);
+    const review = await Review.findById(reviewId);
     if(!review.author.equals(req.user._id)){
         req.flash('error','You do not have permission to do that.');
         res.redirect(`/campgrounds/${campground._id}`);
