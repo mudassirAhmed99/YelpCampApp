106767a4a392c6021dc9148b5035ca1c718744e6
Mudassir Ahmed <ahmed.mudassir1999@gmail.com>
2020-10-21 19:18:28 +0530

    Added functionality to add more images in the edit form and delete images from the mongo databse as well as clodinary.

--- models/campground.js
+++ models/campground.js
@@ -1,15 +1,21 @@
+const { func } = require('joi');
 const mongoose = require('mongoose');
+const { replaceOne } = require('./review');
 const Review = require('./review')
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+});
+
+ImageSchema.virtual('thumbnail').get(function(){
+    return this.url.replace('/upload','/upload/w_200,h_150');
+});
+
 const CampgroundSchema = new Schema({
     title: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     price: Number,
     description: String,
     location: String,
