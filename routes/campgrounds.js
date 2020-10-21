33ace81590577b2159dd84bd3d39dc900f4bf0da
Mudassir Ahmed <ahmed.mudassir1999@gmail.com>
2020-10-21 17:13:52 +0530

    Changed new campground form to take multiple image files, added and configured cloudinary to store image files.

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const multer = require('multer');
+const { storage } = require('../cloudinary/index');
+const upload = multer({ storage });
 const catchAsync = require('../utils/catchAsync');
 const { isLoggedIn, validateCampground, isAuthor } = require('../middleware');
 const campgrounds = require('../controllers/campgrounds');
+
 
 const Campground = require('../models/campground');
 
 router.route('/')
     .get(catchAsync(campgrounds.index))
-    .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    // .post(isLoggedIn, validateCampground, catchAsync(campgrounds.createCampground))
+    .post(upload.array('image'), (req,res) => {
+        console.log(req.body, req.files);
+        res.send("IT WORKED!")
+    })
  
 router.get('/new', isLoggedIn, campgrounds.renderNewForm)
 
