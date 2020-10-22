89b50eb234de97407245922b3cbffe282e0b272d
Mudassir Ahmed <ahmed.mudassir1999@gmail.com>
2020-10-22 15:21:19 +0530

    Changed styling of the cluster map.

--- seeds/index.js
+++ seeds/index.js
@@ -1,17 +1,17 @@
 const Campground = require('../models/campground');
 const mongoose = require('mongoose');
 const cities = require('./cities');
-const {places, descriptors} = require('./seedHelpers');
+const { places, descriptors } = require('./seedHelpers');
 
-mongoose.connect('mongodb://localhost:27017/yelp-camp',{
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true
 });
 
 const db = mongoose.connection;
-db.on('error',console.error.bind(console,'connection error:'));
-db.once('open',() => {
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
     console.log('DATABASE CONNECTED');
 });
 
@@ -19,12 +19,12 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for(let i = 0 ; i < 50; i++){
+    for (let i = 0; i < 200; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
         const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             author: '5f8927f9cf90630ee8ff1e80',
-            location : `${cities[random1000].city}, ${cities[random1000].state}`,
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             geometry: {
                 type: "Point",
@@ -35,14 +35,14 @@ const seedDB = async () => {
             },
             images: [
                 {
-                  url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png',
-                  filename: 'YelpCamp/ahfnenvca4tha00h2ubt'
+                    url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ahfnenvca4tha00h2ubt.png',
+                    filename: 'YelpCamp/ahfnenvca4tha00h2ubt'
                 },
                 {
                     url: 'https://res.cloudinary.com/douqbebwk/image/upload/v1600060601/YelpCamp/ruyoaxgf72nzpi4y6cdi.png',
                     filename: 'YelpCamp/ruyoaxgf72nzpi4y6cdi'
                 }
-              ],
+            ],
             description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Quas necessitatibus ea dolores ullam sapiente alias, cumque vitae, nam earum similique iste architecto et quo quam dolorum quibusdam, vel excepturi doloremque.',
             price: `${price}`
         })
